8bdcc1e60f11b2aa9b80a52872fdb8093ea1e3fe
Ciaran Madden <Ciaran.Madden@infrontconsulting.com>
2019-03-27 12:41:55 +0000

    removed addeploy process kill

--- AzureResourceGroup1/Scripts/DeployAD.ps1
+++ AzureResourceGroup1/Scripts/DeployAD.ps1
@@ -109,7 +109,7 @@ if (!(Test-Path -Path "$($completeFile)$step")) {
     $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
     Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
     Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
-    Stop-Process -Name Explorer
+    #Stop-Process -Name Explorer
 
 	# Unbind IPv6 address
 	$adapter | Disable-NetAdapterBinding –ComponentID ms_tcpip6
