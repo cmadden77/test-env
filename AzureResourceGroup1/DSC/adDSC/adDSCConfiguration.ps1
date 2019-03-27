4f9d285a24857fd70e0c1d365b9181bde337dd67
Ciaran Madden <Ciaran.Madden@infrontconsulting.com>
2019-03-27 21:33:23 +0000

    pki and dsc fixes

--- AzureResourceGroup1/DSC/adDSC/adDSCConfiguration.ps1
+++ AzureResourceGroup1/DSC/adDSC/adDSCConfiguration.ps1
@@ -888,98 +888,7 @@ configuration RootCA
 				}
 			}
 		}
-<#
-        Script InstallAADConnect
-        {
-            SetScript = {
-                $AADConnectDLUrl="https://download.microsoft.com/download/B/0/0/B00291D0-5A83-4DE7-86F5-980BC00DE05A/AzureADConnect.msi"
-                $exe="$env:SystemRoot\system32\msiexec.exe"
 
-                $tempfile = [System.IO.Path]::GetTempFileName()
-                $folder = [System.IO.Path]::GetDirectoryName($tempfile)
-
-                $webclient = New-Object System.Net.WebClient
-                $webclient.DownloadFile($AADConnectDLUrl, $tempfile)
-
-                Rename-Item -Path $tempfile -NewName "AzureADConnect.msi"
-                $MSIPath = $folder + "\AzureADConnect.msi"
-
-                Invoke-Expression "& `"$exe`" /i $MSIPath /qn /passive /forcerestart"
-            }
-
-            GetScript =  { @{} }
-            TestScript = { 
-                return Test-Path "$env:TEMP\AzureADConnect.msi" 
-            }
-        }
-
-        Script CreateOU
-        {
-            SetScript = {
-                $wmiDomain = Get-WmiObject Win32_NTDomain -Filter "DnsForestName = "$( (Get-WmiObject Win32_ComputerSystem).Domain)""
-                $segments = $wmiDomain.DnsForestName.Split(".")
-                $path = [string]::Join(", ", ($segments | % { "DC={0}" -f $_ }))
-                New-ADOrganizationalUnit -Name "OrgUsers" -Path $path
-            }
-            GetScript =  { @{} }
-            TestScript = { 
-                $test=Get-ADOrganizationalUnit -Server "$using:ComputerName.$using:DomainName" -Filter "Name -like "OrgUsers"" -ErrorAction SilentlyContinue
-                return ($test -ine $null)
-            }
-        }
-
-        Script AddTestUsers
-        {
-            SetScript = {
-                $wmiDomain = Get-WmiObject Win32_NTDomain -Filter "DnsForestName = "$( (Get-WmiObject Win32_ComputerSystem).Domain)""
-                $mailDomain=$wmiDomain.DnsForestName
-                $server="$($wmiDomain.PSComputerName).$($wmiDomain.DnsForestName)"
-                $segments = $wmiDomain.DnsForestName.Split(".")
-                $OU = "OU=OrgUsers, {0}" -f [string]::Join(", ", ($segments | % { "DC={0}" -f $_ }))
-                
-                $folder=$using:DscWorkingFolder
-
-				$clearPw = $using:ClearDefUserPw
-				$Users = $using:usersArray
-
-                foreach ($User in $Users)
-                {
-                    $Displayname = $User."FName" + " " + $User."LName"
-                    $UserFirstname = $User."FName"
-                    $UserLastname = $User."LName"
-                    $SAM = $User."SAM"
-                    $UPN = $User."FName" + "." + $User."LName" + "@" + $Maildomain
-                    #$Password = $User."Password"
-                    $Password = $clearPw
-                    "$DisplayName, $Password, $SAM"
-                    New-ADUser `
-                        -Name "$Displayname" `
-                        -DisplayName "$Displayname" `
-                        -SamAccountName $SAM `
-                        -UserPrincipalName $UPN `
-                        -GivenName "$UserFirstname" `
-                        -Surname "$UserLastname" `
-                        -Description "$Description" `
-                        -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) `
-                        -Enabled $true `
-                        -Path "$OU" `
-                        -ChangePasswordAtLogon $false `
-                        -PasswordNeverExpires $true `
-                        -server $server `
-                        -EmailAddress $UPN
-                }
-            }
-            GetScript =  { @{} }
-            TestScript = { 
-				$Users = $using:usersArray
-                $samname=$Users[0]."SAM"
-                $user = get-aduser -filter {SamAccountName -eq $samname} -ErrorAction SilentlyContinue
-                return ($user -ine $null)
-            }
-            DependsOn  = "[Script]CreateOU"
-        }
-		
-#>
         #using service credentials for ADFS for now
 		Script AddTools
         {
@@ -1080,7 +989,7 @@ configuration ADConnect
         }
 
 		
-#>
+
         #using service credentials for ADFS for now
 		Script AddTools
         {
